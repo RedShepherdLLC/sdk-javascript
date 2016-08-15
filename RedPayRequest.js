df1b49df9867c0f28897fcf4e6ca34a6175ab3f2
Khalid Tawil <tawilk2014@gmail.com>
2016-08-15 14:51:08 -0700

    Initial commit

---
+++ RedPayRequest.js
@@ -0,0 +1,9 @@
+function RedPayRequest(action, amount, account, cardHolderName, expmmyyyy, cvv, avsZip) {
+  this.action = action;
+  this.amout = amount;
+  this.account = account;
+  this.cardHolderName = cardHolderName;
+  this.expmmyyyy = expmmyyyy;
+  this.cvv = cvv;
+  this.avsZip;
+}
