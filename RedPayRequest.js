c252ecc791e624588b3bc939a2ed649f20a111b9
Khalid Tawil <tawilk2014@gmail.com>
2016-08-15 15:01:54 -0700

    Added RedPayRequest object

--- RedPayRequest.js
+++ RedPayRequest.js
@@ -1,9 +1,12 @@
 function RedPayRequest(action, amount, account, cardHolderName, expmmyyyy, cvv, avsZip) {
   this.action = action;
-  this.amout = amount;
+  this.amount = amount;
   this.account = account;
   this.cardHolderName = cardHolderName;
   this.expmmyyyy = expmmyyyy;
   this.cvv = cvv;
-  this.avsZip;
+  this.avsZip = avsZip;
 }
+
+var theRequest = new RedPayRequest("A", 5000, "411", "John Doe", "042018", "344", "04233");
+console.log("theRequest: ", theRequest);
