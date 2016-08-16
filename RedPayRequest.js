b260cf446be60d0547f716aa3c87ff2802dc8f70
Khalid Tawil <tawilk2014@gmail.com>
2016-08-15 17:53:01 -0700

    Cleaned up charge methods

--- RedPayRequest.js
+++ RedPayRequest.js
@@ -2,29 +2,33 @@ function RedPayRequest({action, amount, account, currency, expmmyyyy, cvv, signa
  cardHolderName, method, authCode, retryCount, track1Data, track2Data, avsAddress1,
  avsAddress2, avsCity, avsZip, cardHolderEmail, cardHolderPhone, employeeRefNum,
  customerRefNum, orderRefNum, terminalRefNum, transactionId}) {
-  this.action = action;
-  this.amount = amount;
-  this.account = account;
-  this.cardHolderName = cardHolderName;
-  this.expmmyyyy = expmmyyyy;
-  this.cvv = cvv;
-  this.avsZip = avsZip;
-  this.currency = currency;
-  this.method = method;
-  this.retryCount = retryCount;
-  this.avsAddress1 = avsAddress1;
-  this.avsAddress2 = avsAddress2;
-  this.avsCity = avsCity;
-  this.cardHolderEmail = cardHolderEmail;
-  this.cardHolderPhone = cardHolderPhone;
-  this.employeeRefNum = employeeRefNum;
-  this.customerRefNum = customerRefNum;
-  this.orderRefNum = orderRefNum;
-  this.terminalRefNum = terminalRefNum;
-  this.transactionId = transactionId;
+  // this.action = action;
+  // this.amount = amount;
+  // this.account = account;
+  // this.cardHolderName = cardHolderName;
+  // this.expmmyyyy = expmmyyyy;
+  // this.cvv = cvv;
+  // this.avsZip = avsZip;
+  // this.currency = currency;
+  // this.method = method;
+  // this.retryCount = retryCount;
+  // this.avsAddress1 = avsAddress1;
+  // this.avsAddress2 = avsAddress2;
+  // this.avsCity = avsCity;
+  // this.cardHolderEmail = cardHolderEmail;
+  // this.cardHolderPhone = cardHolderPhone;
+  // this.employeeRefNum = employeeRefNum;
+  // this.customerRefNum = customerRefNum;
+  // this.orderRefNum = orderRefNum;
+  // this.terminalRefNum = terminalRefNum;
+  // this.transactionId = transactionId;
+  var somePacket = new Packet(config.app, arguments[0], "45");
+  var someStringPacket = JSON.stringify(somePacket);
+  return somePacket;
+
 
   this.create = function() {
-    var somePacket = new Packet(config.app, this, "45");
+    var somePacket = new Packet(config.app, args, "45");
     var someStringPacket = JSON.stringify(somePacket);
     return somePacket;
   }
